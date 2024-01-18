0cbb49ddf39bb67253dcfe8691241864eae2cd2d
Fco. Javier Cano Moreno <francano368@uma.es>
2024-01-18 11:20:03 +0100

    Update main.rs

--- raspberry/src/main.rs
+++ raspberry/src/main.rs
@@ -5,7 +5,7 @@ mod utils;
 
 
 fn main() {
-    let ip = "192.168.1.133";
+    let ip = "192.168.1.141";
     let port = "3000";
     let device_id = 1;
     let res = sends::temperature::send_temperature(ip.to_string(), port.to_string(), device_id);
