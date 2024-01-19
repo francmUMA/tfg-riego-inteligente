b5768eceb840090450695b77c6c60728305ac699
Fco. Javier Cano Moreno <francano368@uma.es>
2024-01-19 20:43:06 +0100

    frontend init

---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"bWMYFJWZyf6LO2WY9bzWSI+lWEav4hs7YczGVeaSvDw=\"\n}"
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "./ui/login/loginForm";
+
+export default function Page() {
+    return (
+        <div className="">
+            <LoginForm/>
+        </div>
+    )
+}
