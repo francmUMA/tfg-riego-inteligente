c7f44d57aa458e8d7102edcbcc04f8754d9c452d
francm9 <francano368@gmail.com>
2024-01-24 14:20:36 +0100

    nuevo metodo

--- backend/users/routes/userRoutes.js
+++ backend/users/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { add_user, get_user_by_email, get_user_by_nif, check_password } from '../controllers/UserController.js'
+import { add_user, get_user_by_email, get_user_by_nif, check_password, update_user } from '../controllers/UserController.js'
 
 const router = express.Router()
 router.post('/', add_user)
 router.get('/nif/:NIF', get_user_by_nif)
 router.get('/email/:email', get_user_by_email)
 router.post('/password-check', check_password)
+router.put('/', update_user)
 
 export default router
