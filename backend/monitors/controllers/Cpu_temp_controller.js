103c62103a5cf2204e4f2211a2b62439d22390e6
Fco. Javier Cano Moreno <francano368@uma.es>
2024-02-04 11:37:37 +0100

    changes

--- backend/monitors/controllers/Cpu_temp_controller.js
+++ backend/monitors/controllers/Cpu_temp_controller.js
@@ -1,4 +1,5 @@
 import Cpu_temp_Model from '../models/Cpu_temp_model.js';
+import { get_nif_by_token } from '../../users/controllers/UserController.js';
 
 export const add_value = async (req, res) => {
     try {
@@ -7,4 +8,43 @@ export const add_value = async (req, res) => {
     } catch (error) {
         res.status(500).send(error.message)
     }
+}
+
+export const get_values_by_device = async (req, res) => {
+    // Validar token
+    let nif
+    try {
+        nif = await get_nif_by_token(req.header('Authorization').replace('Bearer ', ''))
+    } catch (error) {
+        res.status(401).send("Invalid token")
+        return
+    }
+
+    if (nif === undefined) {
+        res.status(401).send("Invalid token")
+        return
+    }
+
+    // ------------------- POSIBLES ERRORES --------------------
+    if (req.params.id === undefined || req.params.id == "") {
+        res.status(400).send("Missing id")
+        return
+    }
+    // ----------------------------------------------------------
+    try {
+        let values = await Cpu_temp_Model.findAll({ where: { device: req.params.id } })
+        if (values === null) {
+            res.status(404).send("Values not found")
+            return
+        }
+        // Enviar solamente time y value
+        let response = []
+        for (let i = 0; i < values.length; i++) {
+            response.push({ time: values[i].time, value: values[i].value })
+        }
+        res.status(200).send(response)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+
 }
--- backend/monitors/routes/Cpu_temp_route.js
+++ backend/monitors/routes/Cpu_temp_route.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { add_value } from '../controllers/Cpu_temp_controller.js'
+import { add_value,get_values_by_device } from '../controllers/Cpu_temp_controller.js'
 
 const router = express.Router()
 router.post('/', add_value)
+router.get('/all/:id', get_values_by_device)
 
 export default router
