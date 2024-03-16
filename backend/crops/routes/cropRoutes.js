78041cbd24532f0fbd5bc29de2b063bda3f5f61d
Fco. Javier Cano Moreno <francano368@uma.es>
2024-03-16 22:05:19 +0100

    Update cropRoutes.js

--- backend/crops/routes/cropRoutes.js
+++ backend/crops/routes/cropRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createCrop, deleteCrop, getCrops, updateCrop, getCrop } from "../controllers/cropControllers.js";
+import { createCrop, deleteCrop, getCrops, updateCrop, getCrop, getCropAreas } from "../controllers/cropControllers.js";
 
 const router = express.Router();
 router.post('/', createCrop);
 router.get('/', getCrops);
 router.get('/:id', getCrop);
+router.get('/areas/:id', getCropAreas);
 router.put('/', updateCrop);
 router.delete('/', deleteCrop);
 
