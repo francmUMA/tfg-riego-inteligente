6a52e6e32a9c74a0d1845c7fa7f8d05a11a88a0c
Fco. Javier Cano Moreno <francano368@uma.es>
2024-03-25 17:19:25 +0100

    Update main.rs

--- raspberry/src/main.rs
+++ raspberry/src/main.rs
@@ -22,25 +22,24 @@ fn main() {
     // }
     use crate::device::info;
     let token = get_token("test@gmail.com".to_string(), "test_pass".to_string()); 
-    if token != None {
-        println!("{}", token.unwrap());
-    } else {
+    if token.is_none() {
         println!("Error al obtener el token");
+        return;
     }
-    // let uuid = get_my_uuid();
-    // loop {
-    //     let device = info::get_device_info(uuid.clone());
-    //     if let Err(_) = device {
-    //         println!("Error al obtener la información del dispositivo");
-    //     } else {
-    //         println!("Información del dispositivo obtenida");
-    //     }
-    //     let sensors = sensors::get_sensors_device(uuid.clone(), token.clone());
-    //     if let Err(_) = sensors {
-    //         println!("Error al obtener la información de los sensores");
-    //     } else {
-    //         println!("Sensor 1: {}", sensors.unwrap()[0].get_id());
-    //     }
-    //     std::thread::sleep(std::time::Duration::from_secs(60));
-    // }
+    let uuid = get_my_uuid();
+    loop {
+        let device = info::get_device_info(uuid.clone());
+        if let Err(_) = device {
+            println!("Error al obtener la información del dispositivo");
+        } else {
+            println!("Información del dispositivo obtenida");
+        }
+        let sensors = sensors::get_sensors_device(uuid.clone(), token.clone().unwrap());
+        if let Err(_) = sensors {
+            println!("Error al obtener la información de los sensores");
+        } else {
+            println!("Sensor 1: {}", sensors.unwrap()[0].get_id());
+        }
+        std::thread::sleep(std::time::Duration::from_secs(60));
+    }
 }
