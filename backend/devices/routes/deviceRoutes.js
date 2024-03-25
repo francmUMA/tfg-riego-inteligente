cc33837af3b24fb3d83bb8830b54394bfcc0ff8a
Fco. Javier Cano Moreno <francano368@uma.es>
2024-03-25 12:49:11 +0100

    Update deviceRoutes.js

--- backend/devices/routes/deviceRoutes.js
+++ backend/devices/routes/deviceRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { getDevices,addDevice,deleteDevice,updatePosition, updateIp, testConnection, getDevice, updateArea, updateDeviceName, getDeviceUuid } from "../controllers/deviceController.js";
+import { getDevices,addDevice,deleteDevice,updatePosition, updateIp, testConnection, getDevice, updateArea, updateDeviceName, getDeviceUuid, getDeviceByUuid } from "../controllers/deviceController.js";
 
 const router = express.Router();
 
 router.get("/", getDevices)
 router.get("/id/:id", getDevice)
 router.get("/uuid", getDeviceUuid)
-router.get("/uuid/:uuid", getDeviceUuid)
+router.get("/uuid/:uuid", getDeviceByUuid)
 router.post("/", addDevice)
 router.delete("/:id", deleteDevice)
 router.put("/position", updatePosition)
