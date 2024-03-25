c017f2106a5ac9e3b59b4faef12e5c7548379080
Fco. Javier Cano Moreno <francano368@uma.es>
2024-03-25 13:59:10 +0100

    check values on creation

--- raspberry/src/device/sensors.rs
+++ raspberry/src/device/sensors.rs
@@ -1,3 +1,5 @@
+use serde_json::Value;
+
 pub struct  Sensor {
     id: String,
     device: String,
@@ -15,25 +17,45 @@ impl Sensor {
     pub fn new(
         id: String,
         device: String,
-        device_pin: u8,
+        device_pin: Value,
         sensor_type: String,
-        area: String,
-        latitud: f64,
-        longitud: f64,
+        area: Value,
+        latitud: Value,
+        longitud: Value,
         name: String,
-        value: u32,
+        value: Value,
         available: u8
     ) -> Sensor {
+        let mut device_pin_value: Option<u8> = None;
+        let mut area_value: Option<String> = None;
+        let mut latitud_value: Option<f64> = None;
+        let mut longitud_value: Option<f64> = None;
+        let mut value_value: Option<u32> = None;
+        if device_pin.as_u64().is_some() {
+            device_pin_value = Some(device_pin.as_u64().unwrap() as u8);
+        }
+        if area.is_string() {
+            area_value = Some(area.to_string());
+        }
+        if latitud.is_f64() {
+            latitud_value = Some(latitud.as_f64().unwrap());
+        }
+        if longitud.is_f64() {
+            longitud_value = Some(longitud.as_f64().unwrap());
+        }
+        if value.is_u64() {
+            value_value = Some(value.as_u64().unwrap() as u32);
+        }
         Sensor {
             id,
             device,
-            device_pin,
+            device_pin: device_pin_value.unwrap() as u8,
             sensor_type,
-            area,
-            latitud,
-            longitud,
+            area: area_value.unwrap(),
+            latitud: latitud_value.unwrap(),
+            longitud: longitud_value.unwrap(),
             name,
-            value,
+            value: value_value.unwrap(),
             available
         }
     }
@@ -72,13 +94,13 @@ pub async fn get_sensors_device(uuid: String, token: String) -> Result<Vec<Senso
             let sensor = Sensor::new(
                 sensor["id"].to_string(),
                 sensor["device"].to_string(),
-                sensor["device_pin"].as_u64().unwrap() as u8,
+                sensor["device_pin"].clone(),
                 sensor["type"].to_string(),
-                sensor["area"].to_string(),
-                sensor["Latitud"].as_f64().unwrap(),
-                sensor["Longitud"].as_f64().unwrap(),
+                sensor["area"].clone(),
+                sensor["Latitud"].clone(),
+                sensor["Longitud"].clone(),
                 sensor["name"].to_string(),
-                sensor["value"].as_u64().unwrap() as u32,
+                sensor["value"].clone(),
                 sensor["available"].as_u64().unwrap() as u8
             );
             sensors.push(sensor);
