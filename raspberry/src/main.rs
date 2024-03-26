b96cb969dde63fab8b8682f8d19c3f3c1fabca5b
Fco. Javier Cano Moreno <francano368@uma.es>
2024-03-26 10:33:00 +0100

    Update main.rs

--- raspberry/src/main.rs
+++ raspberry/src/main.rs
@@ -100,7 +100,7 @@ fn main() {
 
     // Mostrar mensajes recibidos
     loop {
-        if let Ok(msg) = data.recv() {
+        if let Ok(msg) = data.recv().unwrap() {
             let topic = msg.topic();
             let payload = msg.payload_str();
             println!("Topic: {}, Payload: {}", topic, payload);
