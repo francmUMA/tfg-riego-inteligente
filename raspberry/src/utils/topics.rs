340ef0638549a7a6f4cd8b9b22a1206da9e5f4ff
Fco. Javier Cano Moreno <francano368@uma.es>
2024-03-26 17:50:07 +0100

    Update topics.rs

--- raspberry/src/utils/topics.rs
+++ raspberry/src/utils/topics.rs
@@ -52,7 +52,7 @@ pub fn manage_msg(topic: &str, payload: &str, device: &mut Device, actuadores: &
                 println!("Actuadores obtenidos");
                 println!("Tenemos los siguientes actuadores:");
                 actuadores.clear(); // Limpiar los actuadores existentes
-                actuadores.extend(actuadores_fetch.iter());
+                actuadores.extend(actuadores_fetch.iter().as_ref());
                 for actuador in actuadores.iter() {
                     println!("Actuador con id: {} y nombre: {}", actuador.get_id(), actuador.get_name());
                 }
