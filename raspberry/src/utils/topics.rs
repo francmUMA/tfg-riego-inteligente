e9495858a710c29040692b25a20bbec1534c1458
Fco. Javier Cano Moreno <francano368@uma.es>
2024-03-26 11:51:25 +0100

    Update topics.rs

--- raspberry/src/utils/topics.rs
+++ raspberry/src/utils/topics.rs
@@ -1,7 +1,19 @@
 use crate::device::{actuadores::{self, Actuador}, info::Device};
 
+fn manage_topic_actuadores(topic: &str, payload: &str, actuadores: &mut Vec<Actuador>){
+    println!("Topic de actuadores");
+}
+
+fn manage_topic_device(topic: &str, payload: &str, device: &mut Device){
+    println!("Topic de dispositivos");
+}
+
 pub fn manage_msg(topic: &str, payload: &str, device: &mut Device, actuadores: &mut Vec<Actuador>){
     println!("Mensaje recibido en el topic: {}", topic);
-    println!("Payload: {}", payload);
-    println!("Device: {}", device.get_id());
+    // Hay que saber si el topic es de un actuador o de un dispositivo
+    if topic.contains("actuadores") {
+        manage_topic_actuadores(topic, payload, actuadores);
+    } else {
+        manage_topic_device(topic, payload, device);
+    }
 }
