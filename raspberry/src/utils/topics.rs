72d26636b5771418da286b1013b99abffa3ccd71
Fco. Javier Cano Moreno <francano368@uma.es>
2024-03-26 11:55:38 +0100

    Update topics.rs

--- raspberry/src/utils/topics.rs
+++ raspberry/src/utils/topics.rs
@@ -1,7 +1,17 @@
 use crate::device::{actuadores::{self, Actuador}, info::Device};
 
 fn manage_topic_actuadores(topic: &str, payload: &str, actuadores: &mut Vec<Actuador>){
-    println!("Topic de actuadores");
+    // Se obtiene el id del actuador
+    let topic_split: Vec<&str> = topic.split("/").collect();
+    let actuador_id = topic_split[3];
+
+    // Se obtiene el actuador
+    let actuador = actuadores.iter().find(|actuador| actuador.get_id() == actuador_id);
+    if actuador.is_none() {
+        println!("No se ha encontrado el actuador");
+        return;
+    }
+    println!("Actuador encontrado: {}", actuador_id);
 }
 
 fn manage_topic_device(topic: &str, payload: &str, device: &mut Device){
