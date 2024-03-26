d3b6e7276cea83425eec569ae22c8ab90d348fef
Fco. Javier Cano Moreno <francano368@uma.es>
2024-03-26 16:58:45 +0100

    Update main.rs

--- raspberry/src/main.rs
+++ raspberry/src/main.rs
@@ -84,6 +84,7 @@ fn main() {
     let mut actuadores = actuadores.unwrap();
     for actuador in actuadores.iter() {
         topics.push(format!("devices/{}/actuadores/{}/update/status", device_uuid, actuador.get_id()));
+        topics.push(format!("devices/{}/actuadores/{}/update/device_pin", device_uuid, actuador.get_id()));
     }
     
 
