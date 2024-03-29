9d0efbf67045bc813183d9fb41d9d3ec57c6625b
Fco. Javier Cano Moreno <francano368@uma.es>
2024-03-29 19:58:27 +0100

    Update mqttWorker.js

--- backend/mqttWorker.js
+++ backend/mqttWorker.js
@@ -1,6 +1,6 @@
 import {isMainThread, parentPort} from 'worker_threads'
 import mqtt from "mqtt"
-import sensorsModel from './sensors/models/sensorsModel'
+import sensorsModel from './sensors/models/sensorsModel.js'
 
 if (!isMainThread){
     const client = mqtt.connect(`mqtt://${process.env.BROKER_IP}:1883`)
