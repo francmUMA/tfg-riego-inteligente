9937746b54d4ad291780d2e11975d48e81244cfa
francm9 <francano368@uma.es>
2024-04-04 21:34:05 +0200

    ...

--- backend/monitors/controllers/monitorController.js
+++ backend/monitors/controllers/monitorController.js
@@ -78,7 +78,52 @@ export const addValue = async (data) => {
         }
     }
     console.log("No se ha podido añadir el valor")
-}   
+}
+
+/**
+ * 
+ * @param {
+ *  "id": string
+ * } params  
+ */	
+export const getSensorValue = async (req, res) => {
+    // Validar token
+    let nif
+    try {
+        nif = await get_nif_by_token(req.header('Authorization').replace('Bearer ', ''))
+    } catch (error) {
+        res.status(401).send("Invalid token")
+        return
+    }
+
+    if (nif === undefined) {
+        res.status(401).send("Invalid token")
+        return
+    }
+
+    // ------------------- POSIBLES ERRORES --------------------
+    if (req.params.id === undefined || req.params.id == "") {
+        res.status(400).send("Missing id")
+        return
+    }
+
+    if (!validate(req.params.id)) {
+        res.status(400).send("Invalid id")
+        return
+    }
+
+    // ----------------------------------------------------------
+    try {
+        let last_value = await monitorModel.findOne({ where: { sensorCode: req.params.id }, order: [['time', 'DESC']] })
+        if (last_value === null) {
+            res.status(404).send("Value not found")
+            return
+        }
+        res.status(200).send(last_value)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
 
 export const get_values_by_device = async (req, res) => {
     // Validar token
--- backend/monitors/routes/monitorRoute.js
+++ backend/monitors/routes/monitorRoute.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { get_values_by_device } from '../controllers/monitorController.js'
+import { getSensorValue, get_values_by_device } from '../controllers/monitorController.js'
 
 const router = express.Router()
 router.get('/all/:id', get_values_by_device)
+router.get('/sensor/last/:id', getSensorValue)
 
 export default router
