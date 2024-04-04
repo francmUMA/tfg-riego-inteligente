7c9e6484dca954ccd4843c46d9830d4101fc023f
francm9 <francano368@uma.es>
2024-04-04 12:41:43 +0200

    Update monitorRoute.js

--- backend/monitors/routes/monitorRoute.js
+++ backend/monitors/routes/monitorRoute.js
@@ -1,8 +1,7 @@
 import express from 'express'
-import { add_value,get_values_by_device } from '../controllers/Cpu_temp_controller.js'
+import { get_values_by_device } from '../controllers/monitorController.js'
 
 const router = express.Router()
-router.post('/', add_value)
 router.get('/all/:id', get_values_by_device)
 
 export default router
