7c698eb1d10fd5e648605613d3ac69f9ca11ddd0
francm9 <francano368@uma.es>
2024-04-17 19:01:33 +0200

    Update programsController.js

--- backend/programs/controllers/programsController.js
+++ backend/programs/controllers/programsController.js
@@ -1,9 +1,14 @@
 import { v4 } from "uuid"
+import { get_nif_by_token } from "../../auth/controllers/authController"
 import programsModel from "../models/programsModel"
 
 /**
  * 
- * @param days:
+ * @description
+ * Añade un programa a la base de datos.
+ * 
+ * @param name: string,
+ * @param days: number,
  *  - 1: Lunes
  *  - 2: Martes
  *  - 4: Miercoles
@@ -13,13 +18,10 @@ import programsModel from "../models/programsModel"
  *  - 64: Domingo
  *  @example -> Lunes, Martes y Miercoles: 1 + 2 + 4 = 7
  *  @example -> Todos los dias: 1 + 2 + 4 + 8 + 16 + 32 + 64 = 127
+ * @param duration: number,
+ * @param startTime: unix timestamp
  * 
- * @param {
- *  name: string,
- *  days: number,
- *  duration: number,
- *  startTime: unix timestamp,
- * } req 
+ * 
  */
 export const addProgram = async (req, res) => {
     // --------------- Validacion de token -----------------------
@@ -72,9 +74,47 @@ export const addProgram = async (req, res) => {
             name: req.body.name,
             days: req.body.days,
             startTime: req.body.startTime,
-            duration: req.body.duration
+            duration: req.body.duration,
+            user: nif
         })
         res.status(200).send(uuid)
+    } catch (error) {
+        res.status(500).send("Internal error")
+        return
+    }
+}
+
+
+/**
+ * 
+ * @description 
+ * Obtiene los programas de un usuario.
+ * El nif se coge del token
+ */
+
+export const getUserPrograms = async (req, res) => {
+    // --------------- Validacion de token -----------------------
+    let nif
+    try {
+        addProgram
+        nif = await get_nif_by_token(req.header('Authorization').replace('Bearer ', ''))
+    } catch (error) {
+        res.status(401).send("Invalid token")
+        return
+    }
+
+    if (nif === undefined) {
+        res.status(401).send("Invalid token")
+        return
+    }
+    // -----------------------------------------------------------
+    try {
+        let programs = await programsModel.findAll({
+            where: {
+                user: nif
+            }
+        })
+        res.status(200).send(programs)
     } catch (error) {
         res.status(500).send("Internal error")
         return
