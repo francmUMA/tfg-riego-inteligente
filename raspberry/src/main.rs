6ab45d730059083e644218f497a9890bbe8bfaba
Fco. Javier Cano Moreno <francano368@uma.es>
2024-05-12 13:41:57 +0200

    Update main.rs

--- raspberry/src/main.rs
+++ raspberry/src/main.rs
@@ -244,8 +244,7 @@ fn main() {
                 let tx_clone = tx.clone();
                 let timer_id = uuid::Uuid::new_v4();
                 let _guard = timer.schedule_with_delay(chrono::Duration::seconds(2), move || { tx_clone.send(timer_id.clone().to_string()); } );
-                let timer_wrapper = TimerWrapper::new(timer_id.clone().to_string(), time_now, _guard);
-                std::mem::forget(_guard);
+                let timer_wrapper = TimerWrapper::new(timer_id.clone().to_string(), time_now, _guard.clone());
                 timers_list_clone.lock().unwrap().push(timer_wrapper);
             }
             sleep(Duration::from_secs(5));
