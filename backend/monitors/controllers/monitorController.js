9c93947a23a07a15cbd067ec5eb1f22ee3885683
francm9 <francano368@uma.es>
2024-05-19 20:48:06 +0200

    support for geting values from sensors

--- backend/monitors/controllers/monitorController.js
+++ backend/monitors/controllers/monitorController.js
@@ -221,12 +221,12 @@ export const get_values_by_device = async (req, res) => {
 }
 
 /**
- * 
+ * @description Obtiene las últimas 24h de valores de la temperatura de un sensor
  * @param {
-*  "id": string
+*  id: string
 * } params  
 */	
-export const getSensorLast24hValues = async (req, res) => {
+export const getSensorLast24hValuesTemp = async (req, res) => {
     // Validar token
     let nif
     try {
@@ -256,7 +256,106 @@ export const getSensorLast24hValues = async (req, res) => {
     try {
         let last_value = await monitorModel.findAll({ where: { 
             sensorCode: req.params.id, 
-            time: { [Op.gt]: sequelize.literal(`UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 24 HOUR))`) } 
+            time: { [Op.gt]: sequelize.literal(`UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 24 HOUR))`) },
+            type: 0 
+        } })
+        if (last_value === null) {
+            res.status(404).send("Value not found")
+            return
+        }
+        res.status(200).send(last_value)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+/**
+ * @description Obtiene las últimas 24h de valores de la temperatura de suelo de un sensor
+ * @param {
+*  id: string
+* } params  
+*/	
+export const getSensorLast24hValuesSoilTemp = async (req, res) => {
+    // Validar token
+    let nif
+    try {
+        nif = await get_nif_by_token(req.header('Authorization').replace('Bearer ', ''))
+    } catch (error) {
+        res.status(401).send("Invalid token")
+        return
+    }
+
+    if (nif === undefined) {
+        res.status(401).send("Invalid token")
+        return
+    }
+
+    // ------------------- POSIBLES ERRORES --------------------
+    if (req.params.id === undefined || req.params.id == "") {
+        res.status(400).send("Missing id")
+        return
+    }
+
+    if (!validate(req.params.id)) {
+        res.status(400).send("Invalid id")
+        return
+    }
+
+    // ----------------------------------------------------------
+    try {
+        let last_value = await monitorModel.findAll({ where: { 
+            sensorCode: req.params.id, 
+            time: { [Op.gt]: sequelize.literal(`UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 24 HOUR))`) },
+            type: 1 
+        } })
+        if (last_value === null) {
+            res.status(404).send("Value not found")
+            return
+        }
+        res.status(200).send(last_value)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+/**
+ * @description Obtiene las últimas 24h de valores de la humedad de un sensor
+ * @param {
+*  id: string
+* } params  
+*/	
+export const getSensorLast24hValuesHum = async (req, res) => {
+    // Validar token
+    let nif
+    try {
+        nif = await get_nif_by_token(req.header('Authorization').replace('Bearer ', ''))
+    } catch (error) {
+        res.status(401).send("Invalid token")
+        return
+    }
+
+    if (nif === undefined) {
+        res.status(401).send("Invalid token")
+        return
+    }
+
+    // ------------------- POSIBLES ERRORES --------------------
+    if (req.params.id === undefined || req.params.id == "") {
+        res.status(400).send("Missing id")
+        return
+    }
+
+    if (!validate(req.params.id)) {
+        res.status(400).send("Invalid id")
+        return
+    }
+
+    // ----------------------------------------------------------
+    try {
+        let last_value = await monitorModel.findAll({ where: { 
+            sensorCode: req.params.id, 
+            time: { [Op.gt]: sequelize.literal(`UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 24 HOUR))`) },
+            type: 2 
         } })
         if (last_value === null) {
             res.status(404).send("Value not found")
