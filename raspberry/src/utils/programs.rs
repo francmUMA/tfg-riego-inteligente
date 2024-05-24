147a332e33ca512e86c1a8417c8372e37eac7b04
Fco. Javier Cano Moreno <francano368@uma.es>
2024-05-24 16:47:30 +0200

    Update programs.rs

--- raspberry/src/utils/programs.rs
+++ raspberry/src/utils/programs.rs
@@ -79,15 +79,15 @@ impl Program {
         let minute = datetime.minute();
         let irrigate_day = match weekday {
             Weekday::Mon => self.days & 0b00000001,
-            Weekday::Tue => self.days & 0b00000010 >> 1,
-            Weekday::Wed => self.days & 0b00000100 >> 2,
-            Weekday::Thu => self.days & 0b00001000 >> 3,
-            Weekday::Fri => self.days & 0b00010000 >> 4,
-            Weekday::Sat => self.days & 0b00100000 >> 5,
-            Weekday::Sun => self.days & 0b01000000 >> 6,
+            Weekday::Tue => (self.days & 0b00000010) >> 1,
+            Weekday::Wed => (self.days & 0b00000100) >> 2,
+            Weekday::Thu => (self.days & 0b00001000) >> 3,
+            Weekday::Fri => (self.days & 0b00010000) >> 4,
+            Weekday::Sat => (self.days & 0b00100000) >> 5,
+            Weekday::Sun => (self.days & 0b01000000) >> 6,
         };
+
         if (irrigate_day == 0) {
-            println!("{}",self.days);
             return false;
         }
         
