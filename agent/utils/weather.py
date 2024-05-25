2f4b10f2bcf3e495978f5db735b060b6417552cf
francm9 <francano368@uma.es>
2024-05-25 14:26:32 +0200

    fetching weather

--- agent/utils/weather.py
+++ agent/utils/weather.py
@@ -14,3 +14,9 @@ def get_weather_prediction(latitud, longitud):
     res = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?latlng=' +
                        latitud +"," + longitud +
                         "&key=" + os.getenv('GOOGLE_API_KEY'))
+    if res.status_code != 200:
+        print('Error al solicitar la predicción del clima. Code -> ' + str(res.status_code))
+        return None
+    data = res.json()
+    print(data)
+    pass
