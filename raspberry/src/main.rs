82621af1e1efe5cbe0b2eddf7149aebe5c2fabc2
Fco. Javier Cano Moreno <francano368@uma.es>
2024-06-07 19:59:07 +0200

    Update main.rs

--- raspberry/src/main.rs
+++ raspberry/src/main.rs
@@ -221,8 +221,7 @@ fn main() {
                     }
                     let timer_find = timers_list.lock().unwrap().iter().position(|t| t.get_actuador_id() == actuator.get_id());
                     if timer_find.is_some() {
-                        let timer_find = timers_list.lock().unwrap().get_mut(timer_find.unwrap());
-                        let timer_find = timer_find.unwrap();
+                        let timer_find = timers_list.lock().unwrap().get_mut(timer_find.unwrap()).unwrap();
                         if timer_find.is_timer_to_resume() {
                             println!("Reanudando el timer del actuador {}", actuator.get_id());
                             actuator.open();
