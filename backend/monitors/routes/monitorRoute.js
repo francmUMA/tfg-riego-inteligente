f860459266538f5e7860d4ab136798a1b1ae4c96
Fco. Javier Cano Moreno <francano368@uma.es>
2024-06-11 18:10:35 +0200

    Update monitorRoute.js

--- backend/monitors/routes/monitorRoute.js
+++ backend/monitors/routes/monitorRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAreaMeanHumidity, getAreaMeanSoilHumidity, getAreaMeanSoilTemp, getAreaMeanTemperature, getDeviceTemperatureValues, getSensorLast24hValuesFlow, getSensorLast24hValuesHum, getSensorLast24hValuesSoilHum, getSensorLast24hValuesSoilTemp, getSensorLast24hValuesTemp, getSensorValue, get_values_by_device } from '../controllers/monitorController.js'
+import { getAreaMeanHumidity, getAreaMeanSoilHumidity, getAreaMeanSoilTemp, getAreaMeanTemperature, getDeviceTemperatureLast24hValues, getDeviceTemperatureValues, getSensorLast24hValuesFlow, getSensorLast24hValuesHum, getSensorLast24hValuesSoilHum, getSensorLast24hValuesSoilTemp, getSensorLast24hValuesTemp, getSensorValue, get_values_by_device } from '../controllers/monitorController.js'
 
 const router = express.Router()
 router.get('/all/:id', get_values_by_device)
@@ -10,6 +10,7 @@ router.get('/sensor/last24/humidity/:id', getSensorLast24hValuesHum)
 router.get('/sensor/last24/soilHumidity/:id', getSensorLast24hValuesSoilHum)
 router.get('/sensor/last24/flow/:id', getSensorLast24hValuesFlow)
 router.get('/temperature/all/:deviceId', getDeviceTemperatureValues)
+router.get('/temperature/last24/:deviceId', getDeviceTemperatureLast24hValues)
 router.get('/temperature/area/mean/:areaId', getAreaMeanTemperature)
 router.get('/humidity/area/mean/:areaId', getAreaMeanHumidity)
 router.get('/soilTemp/area/mean/:areaId', getAreaMeanSoilTemp)
