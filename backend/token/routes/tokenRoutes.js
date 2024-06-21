232f4f874e4e97c2ae2bb4f9455ea57d73453e40
francm9 <francano368@uma.es>
2024-06-21 06:37:32 +0200

    ...

--- backend/token/routes/tokenRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { createToken, verifyToken } from '../controllers/tokenController.js'
-
-const router = express.Router()
-router.post('/', createToken)
-router.get('/check', verifyToken)
-
-export default router
